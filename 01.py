c820fe219a44d4a36d5cde06d556c782d1d05288
muratibik <muratibik7@gmail.com>
2017-01-19 12:44:10 +0300

    trying

---
+++ 01.py
@@ -0,0 +1,8 @@
+name=input("what is your name? ")
+print("your name is " + name)
+age=input("enter your age: ")
+age=int(age)
+if a<18:
+    print("you are too young enter here")
+elif a>18:
+    print("wlcome" + name)

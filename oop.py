342c32c4bcc82a6df9c623b54240c9c2c26228b1
muratibik <muratibik7@gmail.com>
2017-01-21 14:37:00 +0300

    oop ornek

---
+++ oop.py
@@ -0,0 +1,16 @@
+class car:
+    def __init__(self, brand, price, color):
+
+        car_1 = car()
+        car_2 = car()
+
+        print("car_1")
+        print("car_2")
+
+        car_1.brand = 'toyota'
+        car_1.price = '50000'
+        car_1.color = 'black'
+
+        car_2.brand = 'chevrolet'
+        car_2.price = '60000'
+        car_2.color = 'grey'

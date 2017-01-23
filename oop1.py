61ec1c2793ae1db754ef8f79abac1ae41b29cdbc
muratibik <muratibik7@gmail.com>
2017-01-23 11:14:13 +0300

    new commit

---
+++ oop1.py
@@ -0,0 +1,8 @@
+class myclass:
+    info = 'this is an example of data with in  Python object'
+    def myMethod(self):
+        print "1 + 1 = 2"
+
+myobject = myclass()
+myobject.myMethod()
+print(myobject.info)

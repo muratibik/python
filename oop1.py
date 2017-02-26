ad3f0ca8ef8c7fcfa81ff1ef3426c7246da6377e
muratibik <muratibik7@gmail.com>
2017-02-26 20:56:54 +0300

    projede son aşamaya geldik

--- oop1.py
+++
@@ -1,8 +0,0 @@
-class myclass:
-    info = 'this is an example of data with in  Python object'
-    def myMethod(self):
-        print "1 + 1 = 2"
-
-myobject = myclass()
-myobject.myMethod()
-print(myobject.info)
